2fa78225db1f6e620df4e3482fc2a48d3969c3a8
Patrick Humphries <patrick.v.humphries@icloud.com>
2018-05-22 15:56:27 -0700

    port

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def home():
     return render_template("index.html")
 
 if __name__ == '__main__':
-    from os import environ
-    app.run(debug=False, host='0.0.0.0', port=environ.get("PORT", 5000))
-    # app.run(debug=False, host='0.0.0.0', port=5000)
+    # from os import environ
+    # app.run(debug=False, host='0.0.0.0', port=environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=5000)
     
