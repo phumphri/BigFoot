47e98bbcc5c6f457214e3caf7440db8a4883c325
Patrick Humphries <patrick.v.humphries@icloud.com>
2018-05-22 16:16:28 -0700

    procfile

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ if __name__ == '__main__':
     # from os import environ
     # app.run(debug=False, host='0.0.0.0', port=environ.get("PORT", 5000))
     # app.run(debug=False, host='0.0.0.0', port=5000)
-    app.run(debug=False, host=None, port=None)
+    app.run(debug=False)
     
